fbb114fb2c46e5695f84440929c635521174bafb
jessi <17324719944@189.cn>
2025-09-16 18:07:02 +0800

    1. 优化默认加密

--- Android-KV/src/main/java/top/jessi/kv/storage/ConcealEncryption.java
+++ Android-KV/src/main/java/top/jessi/kv/storage/ConcealEncryption.java
@@ -5,7 +5,7 @@ import top.jessi.jhelper.enigma.Enigma;
 
 /**
  * 默认使用aes进行加密
- * 加密key为数据的键
+ * 加密key为 MagicGalaxyX9944
  * 加密iv为 994400LeoJessi10
  * 可重写本类自定义加密解密算法
  */
@@ -18,12 +18,12 @@ public class ConcealEncryption implements Encryption {
 
     @Override
     public String encrypt(String key, String plainText) {
-        return Enigma.encryptAes(plainText, key, "994400LeoJessi10");
+        return Enigma.encryptAes(plainText, "MagicGalaxyX9944", "994400LeoJessi10");
     }
 
     @Override
     public String decrypt(String key, String cipherText) {
-        return Enigma.decryptAes(cipherText, key, "994400LeoJessi10");
+        return Enigma.decryptAes(cipherText, "MagicGalaxyX9944", "994400LeoJessi10");
     }
 
 }
